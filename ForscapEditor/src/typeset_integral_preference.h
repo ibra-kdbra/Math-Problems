13398968f2a6e1e69baa7900ce2df807789ef3dc
MetantiConan <sonarfarouq@gmail.com>
2023-09-27 22:50:28 +0300

    integral bounds

---
+++ ForscapEditor/src/typeset_integral_preference.h
@@ -0,0 +1,19 @@
+#ifndef TYPESET_INTEGRAL_PREFERENCE_H
+#define TYPESET_INTEGRAL_PREFERENCE_H
+
+namespace Forscape {
+
+namespace Typeset {
+
+inline bool integral_bounds_vertical = false;
+inline bool display_commas_in_numbers = false;
+
+inline constexpr double INTEGRAL_RATIO = 0.5;
+inline constexpr double INTEGRAL_SHIFT_LEFT = 0.6;
+inline constexpr double INTEGRAL_SHIFT_UP = 0.05;
+
+}
+
+}
+
+#endif // TYPESET_INTEGRAL_PREFERENCE_H
