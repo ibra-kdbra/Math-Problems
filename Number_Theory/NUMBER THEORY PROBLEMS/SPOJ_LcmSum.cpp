859d2566c8d77e2bb81f5410402427901e726ea8
ibra-kdbra <git-repo-develop@outlook.com>
2023-10-16 14:55:55 +0300

    lcum sum

--- Number_Theory/NUMBER THEORY PROBLEMS/SPOJ_LcmSum.cpp
+++ Number_Theory/NUMBER THEORY PROBLEMS/SPOJ_LcmSum.cpp
@@ -1,11 +1,11 @@
 // Given n, calculate the sum LCM(1,n) + LCM(2,n) + .. + LCM(n,n), where LCM(i,n)
 // denotes the Least Common Multiple of the integers i and n.
 
-// Input
+// Input:
 
 // The first line contains T the number of test cases. Each of the next T lines contain an integer n.
 
-// Output
+// Output:
 
 // Output T lines, one for each test case, containing the required sum.
 // Example:
