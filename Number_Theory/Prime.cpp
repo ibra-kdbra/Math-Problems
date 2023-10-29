76cf12e7cfd7b3960c10e8cb0d1e9a16a94d9352
ibra-kdbra <git-repo-develop@outlook.com>
2023-10-30 00:22:23 +0300

    added comments

--- Number_Theory/Prime.cpp
+++ Number_Theory/Prime.cpp
@@ -1,36 +1,56 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-bool isprime(int n)
-{
-    int c=0;
-    for(int i=1;i*i<=n;i++)
-    {
-        if(n%i==0)
-        {
-            if(i*i==n)//for the case of perfect square
-            c+=1;
-            else
-            c+=2;
-        }
+// Check if a number is prime
+bool isprime(int n) {
+  // Initialize a counter to keep track of the number of factors.
+  int c = 0;
+
+  // Iterate over all the numbers from 1 to the square root of n, inclusive.
+  for (int i = 1; i * i <= n; i++) {
+    // If the current number is a factor of n, then increment the counter.
+    if (n % i == 0) {
+      c++;
+
+      // If the current number is the square root of n, then we only need to count it once.
+      if (i * i == n) {
+        continue;
+      }
+
+      // Otherwise, we need to count it twice, since it has two factors (itself and its divisor).
+      else {
+        c++;
+      }
     }
-    if(c==2){  //in this case for 1 and that number is included i.e prime
-    //cout<<n<<endl;
-    return true;}
-    else
-    return false;
+  }
+
+  // If the counter is equal to 2, then the number has only two factors (1 and itself), and is therefore prime.
+  // Otherwise, the number has more than two factors, and is therefore not prime.
+  return c == 2;
 }
-int main()
-{
-    int c,n,i,j,k,x;
-    c=0;
-    cin>>n;
-    for(i=1;i<=n;i++)
-    {
-        if(isprime(i))
-        {
-            c++;
-        }
+
+int main() {
+  // Declare variables.
+  int n, count;
+
+  // Get the input.
+  cin >> n;
+
+  // Initialize the count of prime numbers.
+  count = 0;
+
+  // Iterate over all the numbers from 1 to n, inclusive.
+  for (int i = 1; i <= n; i++) {
+    // Check if the current number is prime.
+    if (isprime(i)) {
+      // If it is, increment the count of prime numbers.
+      count++;
     }
-    cout<<c<<endl;
+  }
+
+  // Print the count of prime numbers.
+  cout << count << endl;
+
+  // Return 0 to indicate successful execution of the program.
+  return 0;
 }
