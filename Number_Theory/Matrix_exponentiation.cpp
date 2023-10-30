48205e79039695eaac140a3e56d75bbe181a7bef
ibra-kdbra <git-repo-develop@outlook.com>
2023-10-30 23:53:24 +0300

    matrix commented

--- Number_Theory/Matrix_exponentiation.cpp
+++ Number_Theory/Matrix_exponentiation.cpp
@@ -1,96 +1,145 @@
+// Include the standard C++ library.
 #include <bits/stdc++.h>
+
+// Use the standard C++ namespace.
 using namespace std;
+
+// Macro to define a long long integer type
 #define ll long long
+
+// Macro to define the modulus
 #define m 1000000000
 
-vector<ll > b,c;
+// Global variables
+vector<ll> b, c;
 ll k;
-vector<vector<ll > > multiply(vector<vector<ll > > A,vector<vector<ll > > B){
-    vector<vector<ll > > C(k+1,vector<ll > (k+1));
-    for(int i=1;i<=k;i++){
-        for(int j=1;j<=k;j++){
-            for(int x=1;x<=k;x++){
-                C[i][j]=(C[i][j]+(A[i][x]*B[x][j])%m)%m;
-            }
-        }
-    }
-    /*for(int i=1;i<=k;i++){
-        for(int j=1;j<=k;j++){
-            cout<<C[i][j]<<" ";
-        }
-        cout<<endl;
-    }*/
-    return C;
-}
-vector<vector<ll > > pow(vector<vector<ll > > T,ll n){//matrix exponentiation
-    if(n==1){
-        return T;
-    }
-    //check for both even and odd cases
-    if(n&1){//odd
-        return multiply(T,pow(T,n-1));
-    }
-    else{//even 
-        vector<vector<ll > > A=pow(T,n/2);
-        return multiply(A,A);
-    }
-}
-ll compute(ll n){//answer what is the Nth term
-    if(n==0){
-        return 0;
-    }
-    if(n<=k){
-        return b[n-1];
-    }
-    vector<ll > F1(k+1);//make F1 vector of K*1 STEP:1
-    for(int i=1;i<=k;i++){
-        F1[i]=b[i-1];
-    }
-    //else you want to make the T matrix indexing from 1  STEP:2
-    vector<vector<ll> > T(k+1,vector<ll > (k+1));
-    //filling of matrix T
-    for(int i=1;i<=k;i++){
-        for(int j=1;j<=k;j++){
-            if(i<k){
-                if(j==i+1){//element next to diagonal is 1
-                    T[i][j]=1;
-                }
-                else{
-                    T[i][j]=0;
-                }
-            }
-            else{
-                T[i][j]=c[k-j];//last row is to be filled in opposite direction
-            }
-        }
-    }
-    vector<vector<ll > > M(k+1,vector<ll > (k+1));
-    M=pow(T,n-1);                  //STEP:3
 
-    ll res = 0;
-    for(int i=1;i<=k;i++){         //STEP:4
-        res = (res + (M[1][i]*F1[i])%m)%m;
+// Function to multiply two matrices A and B of size (k+1) x (k+1) modulo m
+vector<vector<ll>> multiply(vector<vector<ll>> A, vector<vector<ll>> B) {
+  // Create a new matrix C of size (k+1) x (k+1) to store the result.
+  vector<vector<ll>> C(k + 1, vector<ll>(k + 1));
 
+  // Iterate over all the rows and columns of the matrices A, B, and C.
+  for (int i = 1; i <= k; i++) {
+    for (int j = 1; j <= k; j++) {
+      // Calculate the element at row i, column j of the resulting matrix C.
+      for (int x = 1; x <= k; x++) {
+        C[i][j] = (C[i][j] + (A[i][x] * B[x][j]) % m) % m;
+      }
     }
+  }
+
+  // Return the resulting matrix C.
+  return C;
+}
+
+// Function to compute the matrix exponentiation of the matrix T to the power of n modulo m.
+vector<vector<ll>> pow(vector<vector<ll>> T, ll n) {
+  // If the power is 1, then simply return the matrix T.
+  if (n == 1) {
+    return T;
+  }
+
+  // Check if the power is even or odd.
+  if (n & 1) {
+    // If the power is odd, then return the product of T and pow(T, n - 1).
+    return multiply(T, pow(T, n - 1));
+  } else {
+    // If the power is even, then return the square of pow(T, n / 2).
+    vector<vector<ll>> A = pow(T, n / 2);
+    return multiply(A, A);
+  }
+}
+
+// Function to compute the Nth term of the recurrence relation.
+ll compute(ll n) {
+  // If n is 0, then return 0.
+  if (n == 0) {
+    return 0;
+  }
+
+  // If n is less than or equal to k, then return the corresponding element of the vector b.
+  if (n <= k) {
+    return b[n - 1];
+  }
+
+  // Create a new vector F1 of size k + 1 to store the first k terms of the recurrence relation.
+  vector<ll> F1(k + 1);
+  for (int i = 1; i <= k; i++) {
+    F1[i] = b[i - 1];
+  }
+
+  // Create a new matrix T of size k + 1 x k + 1 to represent the recurrence relation.
+  vector<vector<ll>> T(k + 1, vector<ll>(k + 1));
+
+  // Fill the matrix T.
+  for (int i = 1; i <= k; i++) {
+    for (int j = 1; j <= k; j++) {
+      if (i < k) {
+        if (j == i + 1) {
+          // The element next to the diagonal is 1.
+          T[i][j] = 1;
+        } else {
+          T[i][j] = 0;
+        }
+      } else {
+        // The last row is to be filled in the opposite direction.
+        T[i][j] = c[k - j];
+      }
+    }
+  }
+
+  // Compute the matrix exponentiation of T to the power of n - 1 modulo m.
+  vector<vector<ll>> M = pow(T, n - 1);
+
+  // Calculate the Nth term of the recurrence relation.
+  ll res = 0;
+  for (int i = 1; i <= k; i++) {
+    res = (res + (M[1][i] * F1[i]) % m) % m;
+  }
+    //response returned
     return res;
 }
 int main() {
-    ll t,n,x;
-    cin>>t;
-    while(t--){
-        cin>>k;
-        for(int i=0;i<k;i++){
-            cin>>x;
-            b.push_back(x);
-        }
-        for(int i=0;i<k;i++){
-            cin>>x;
-            c.push_back(x);
-        }
-        cin>>n;
-        cout<<compute(n)<<endl;
-        b.clear();
-        c.clear();
+  // Declare variables
+  ll t, n, x;
+
+  // Read the number of test cases from the input
+  cin >> t;
+
+  // Iterate over all the test cases
+  while (t--) {
+    // Read the value of k from the input
+    cin >> k;
+
+    // Clear the vectors b and c
+    b.clear();
+    c.clear();
+
+    // Read the elements of the vector b from the input
+    for (int i = 0; i < k; i++) {
+      cin >> x;
+      b.push_back(x);
     }
 
+    // Read the elements of the vector c from the input
+    for (int i = 0; i < k; i++) {
+      cin >> x;
+      c.push_back(x);
+    }
+
+    // Read the value of n from the input
+    cin >> n;
+
+    // Compute the Nth term of the recurrence relation
+    ll res = compute(n);
+
+    // Print the result to the output
+    cout << res << endl;
+  }
+
+  // Return 0 from the main() function
+  return 0;
 }
+
+
--- Number_Theory/matrix_exponentiation.cpp
+++ Number_Theory/matrix_exponentiation.cpp
@@ -1,51 +1,78 @@
-#include<bits/stdc++.h>
+// Include the <bits/stdc++.h> header file. This header file includes a variety of useful libraries, such as the `vector` and `algorithm` libraries.
+#include <bits/stdc++.h>
+
+// Import all of the names from the `std` namespace into the current scope. This means that you can use functions and classes from the `std` namespace without having to prefix them with `std::`.
 using namespace std;
+
+// Define a macro `pb` to expand to `push_back()`. This macro is useful for adding elements to the end of a vector.
 #define pb push_back
+
+// Define a macro `ll` to expand to `long long int`. This macro is useful for defining variables that need to store large integer values.
 #define ll long long int
 
-void multiply(ll a[2][2],ll m[2][2])
-{
-    ll firstvalue=a[0][0]*m[0][0] + a[0][1]*m[1][0];
-    ll secondvalue=a[0][0]*m[1][0] + a[0][1]*m[1][1];
-    ll thirdvalue=a[1][0]*m[0][0] + a[1][1]*m[1][0];
-    ll fourthvalue=a[1][0]*m[0][1] + a[1][1]*m[1][1];
+// Function to multiply two 2x2 matrices.
+void multiply(ll a[2][2], ll m[2][2]) {
+  // Calculate the element at row 0, column 0 of the resulting matrix.
+  ll firstvalue = a[0][0] * m[0][0] + a[0][1] * m[1][0];
 
-    a[0][0]=firstvalue;
-    a[0][1]=secondvalue;
-    a[1][0]=thirdvalue;
-    a[1][1]=fourthvalue;
-}
-void power(ll a[2][2], ll n)
-{
-    if(n==0 || n==1)
-    return ;
+  // Calculate the element at row 0, column 1 of the resulting matrix.
+  ll secondvalue = a[0][0] * m[1][0] + a[0][1] * m[1][1];
 
-    power(a,n/2);
+  // Calculate the element at row 1, column 0 of the resulting matrix.
+  ll thirdvalue = a[1][0] * m[0][0] + a[1][1] * m[1][0];
 
-    //multiplying a^n/2 * a^n/2
-    multiply(a,a);
+  // Calculate the element at row 1, column 1 of the resulting matrix.
+  ll fourthvalue = a[1][0] * m[0][1] + a[1][1] * m[1][1];
 
-    if(n%2!=0)
-    {
-        ll m[2][2]={{1,1},{1,0}};
-        multiply(a,m);
-    }
+  // Update the matrix a with the new values.
+  a[0][0] = firstvalue;
+  a[0][1] = secondvalue;
+  a[1][0] = thirdvalue;
+  a[1][1] = fourthvalue;
 }
 
-ll febo(ll n)
-{
-    ll a[2][2]={{1,1},{1,0}};//find using recurrence  relation
-    if(n==0)
+// Function to raise a 2x2 matrix to the power of n.
+void power(ll a[2][2], ll n) {
+  // If n is 0 or 1, then simply return.
+  if (n == 0 || n == 1) {
+    return;
+  }
+
+  // Recursively compute the power of n / 2.
+  power(a, n / 2);
+
+  // Multiply the matrix a by itself n / 2 times.
+  multiply(a, a);
+
+  // If n is odd, then multiply the matrix a by the matrix m.
+  if (n % 2 != 0) {
+    ll m[2][2] = {{1, 1}, {1, 0}};
+    multiply(a, m);
+  }
+}
+
+// Function to compute the Nth fibonacci number.
+ll febo(ll n) {
+  // If n is 0, then return 0.
+  if (n == 0) {
     return 0;
+  }
 
-    power(a,n-1);
-    return a[0][0];
+  // Create a 2x2 matrix a to represent the recurrence relation.
+  ll a[2][2] = {{1, 1}, {1, 0}};
+
+  // Compute the power of the matrix a to the power of n - 1.
+  power(a, n - 1);
+
+  // Return the element at row 0, column 0 of the resulting matrix.
+  return a[0][0];
 }
 
-int main()
-{
-	ll a,b,c,n;
-//	cin>>n;
-	cout<<febo(0)<<endl;
-	return 0;
+// Main function.
+int main() {
+  // Print the 0th Fibonacci number.
+  cout << febo(0) << endl;
+
+  // Return 0 from the main() function.
+  return 0;
 }
