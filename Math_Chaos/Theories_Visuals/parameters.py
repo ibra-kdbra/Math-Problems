1594e228d06e3f330cb101e73441f3e1d14d9f7b
ibra-kdbra <git-repo-develop@outlook.com>
2023-12-09 23:51:14 +0300

    params to edit before running

---
+++ Math_Chaos/Theories_Visuals/parameters.py
@@ -0,0 +1,13 @@
+# Parameters for the chaotic systems. Change them as you wish
+
+# Lorenz Attractor Parameters
+
+sigma = 10.0        # Sigma value for Lorenz system
+rho = 28.0          # Rho value for Lorenz system
+beta = 8.0/3        # Beta value for Lorenz system
+
+
+# Logistic Map Parameters
+
+inv = 2.4       # Start value for r in logistic map
+endv = 4.0      # End value for r in logistic map
