a9986b0bdf3d9995376d01d268b8a21f38ac05fc
ibra-kdbra <git-repo-develop@outlook.com>
2024-01-12 00:07:46 +0300

    signature run performance

---
+++ Security_Algorithms/Cryptography-Analysis/Signature_Schemes/signature.h
@@ -0,0 +1,6 @@
+#ifndef DISSERTATION_PROJECT_SIGNATURE_H
+#define DISSERTATION_PROJECT_SIGNATURE_H
+
+int run_signature_scheme_performance_analysis(const char *alg_name, uint8_t *message_to_sign);
+
+#endif //DISSERTATION_PROJECT_SIGNATURE_H
