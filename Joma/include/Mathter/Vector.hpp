935d51cd35a182fc3fa4adc78f1486f820febf20
MetantiConan <sonarfarouq@gmail.com>
2024-01-18 23:51:08 +0300

    main include file headers

---
+++ Joma/include/Mathter/Vector.hpp
@@ -0,0 +1,12 @@
+﻿// L=============================================================================
+// L This software is distributed under the MIT license.
+// L Copyright 2022 ibra-kdbra
+// L=============================================================================
+
+#pragma once
+
+#include "Vector/VectorArithmetic.hpp"
+#include "Vector/VectorCompare.hpp"
+#include "Vector/VectorConcat.hpp"
+#include "Vector/VectorFunction.hpp"
+#include "Vector/VectorImpl.hpp"
