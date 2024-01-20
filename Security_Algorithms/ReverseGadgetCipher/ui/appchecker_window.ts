faaa0cd7c445f769dfe2818a4976d6f3f6d4b96e
ibra-kdbra <git-repo-develop@outlook.com>
2024-01-20 15:47:06 +0300

    app checker component

---
+++ Security_Algorithms/ReverseGadgetCipher/ui/appchecker_window.ts
@@ -0,0 +1,65 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>AppcheckerWindow</name>
+    <message>
+        <location filename="appchecker_window.ui" line="19"/>
+        <source>APK Checker</source>
+        <translation type="unfinished">APK hardening detection</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="245"/>
+        <source>Check</source>
+        <translation type="unfinished">Detection</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="115"/>
+        <source>Open APK</source>
+        <translation type="unfinished">Select a document</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="321"/>
+        <source>Target SDK Version</source>
+        <translation type="unfinished">Target SDK version</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="345"/>
+        <source>Min SDK Version</source>
+        <translation type="unfinished">Minimum SDK version</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="265"/>
+        <source>Version name</source>
+        <translation type="unfinished">APP version</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="275"/>
+        <source>Package name</source>
+        <translation type="unfinished">Package names</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="252"/>
+        <source>Manifest.xml</source>
+        <translation type="unfinished">Manifest.xml</translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="54"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Microsoft Yahei&apos;,&apos;Microsoft Yahei&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Microsoft Yahei&apos;; font-size:9.07563pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="appchecker_window.ui" line="139"/>
+        <source>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Microsoft Yahei&apos;,&apos;Microsoft Yahei&apos;; font-size:9pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:9.07563pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
