7a8e293ee99fd7d4112c664825a74374082acc3c
ibra-kdbra <git-repo-develop@outlook.com>
2024-01-21 18:41:42 +0300

    main window component

---
+++ Security_Algorithms/ReverseGadgetCipher/ui/main_window.ts
@@ -0,0 +1,102 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="main_window.ui" line="389"/>
+        <source>Reverse Widget</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="301"/>
+        <source>File Hash</source>
+        <translation type="unfinished">file hash</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="268"/>
+        <source>Format Json</source>
+        <translation type="unfinished">Format Json</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="459"/>
+        <source>File</source>
+        <translation type="unfinished">document</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="463"/>
+        <source>Language</source>
+        <translation type="unfinished">language</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="475"/>
+        <source>Help</source>
+        <translation type="unfinished">help</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="481"/>
+        <source>Tools</source>
+        <translation type="unfinished">tool</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="494"/>
+        <source>English</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="499"/>
+        <source>Chinese</source>
+        <translation type="unfinished">Simplified Chinese</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="504"/>
+        <source>About</source>
+        <translation type="unfinished">about</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="509"/>
+        <source>Open</source>
+        <translation type="unfinished">Open</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="514"/>
+        <source>Font</source>
+        <translation type="unfinished">font</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="523"/>
+        <source>APK Checker</source>
+        <translation type="unfinished">APK hardening detection</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="532"/>
+        <source>PE Checker</source>
+        <translation type="unfinished">PE packing detection</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="541"/>
+        <source>dll injector</source>
+        <translation type="unfinished">dll injection</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="235"/>
+        <source>Format Comment</source>
+        <translation type="unfinished">formatted translation</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="124"/>
+        <source>Crypto</source>
+        <translation type="unfinished">Encryption and decryption</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="160"/>
+        <source>Code</source>
+        <translation type="unfinished">Codec</translation>
+    </message>
+    <message>
+        <location filename="main_window.ui" line="196"/>
+        <source>Assemble</source>
+        <translation type="unfinished">Assembly and disassembly</translation>
+    </message>
+</context>
+</TS>
