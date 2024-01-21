c0532636f6a8cbdfd7334cd9c1e43d196cd6942f
ibra-kdbra <git-repo-develop@outlook.com>
2024-01-21 18:37:02 +0300

    pecheker component

---
+++ Security_Algorithms/ReverseGadgetCipher/ui/pechecker_window.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="zh_CN">
+<context>
+    <name>PecheckerWindow</name>
+    <message>
+        <location filename="pechecker_window.ui" line="14"/>
+        <source>PE Checker</source>
+        <translation type="unfinished">Windows program packing detection</translation>
+    </message>
+    <message>
+        <location filename="pechecker_window.ui" line="63"/>
+        <source>Check</source>
+        <translation type="unfinished">Detection</translation>
+    </message>
+    <message>
+        <location filename="pechecker_window.ui" line="92"/>
+        <source>Open PE</source>
+        <translation type="unfinished">Open PE</translation>
+    </message>
+</context>
+</TS>
