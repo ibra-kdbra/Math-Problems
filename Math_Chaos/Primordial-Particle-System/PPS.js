bc17896b2243d275aecdd1e414e532df8acd311d
ibra-kdbra <git-repo-develop@outlook.com>
2024-02-28 23:45:33 +0300

    Advance simulation by one step (this function is called f times per second)

--- Math_Chaos/Primordial-Particle-System/PPS.js
+++ Math_Chaos/Primordial-Particle-System/PPS.js
@@ -192,3 +192,141 @@ function initialize() {
       console.log('r='+r+'\nv='+v+'\ns='+sr+'\ng='+g);                             // Display values in the console
     }
   
+        
+  // Advance simulation by one step (this function is called f times per second)
+  function step() {
+    count++;
+    if (analyse) {
+      hl-=0.005*(hl-100);   // Gradually shorten histograms
+      hist[0]=new Array();  // Number of neighbours (N)
+      hist[1]=new Array();  // Neighbours on left (L)
+      hist[2]=new Array();  // Neighbours on right (R)
+      hist[3]=new Array();  // Grid cell densities
+    }
+    
+    // Create new grid
+    let xCells=Math.floor(vw/r), xSize=vw/xCells;
+    let yCells=Math.floor(vh/r), ySize=vh/yCells;
+    let grid=new Array(xCells);
+    for (i=0;i<xCells;i++) {
+      grid[i]=new Array(yCells);
+      for (j=0;j<yCells;j++) grid[i][j]=new Array();
+    }
+    
+    for (i=0; i<n; i++) {
+      // Insert particle index into a grid cell
+      let xg=Math.floor(p[i].x/xSize);
+      let yg=Math.floor(p[i].y/ySize);
+      grid[xg][yg].push(i);
+      // Calculate sine and cosine of phi
+      p[i].pSin=Math.sin(p[i].phi);
+      p[i].pCos=Math.cos(p[i].phi);
+      // Reset values of N, L and R
+      p[i].N=0, p[i].L=0, p[i].R=0;
+    }
+    
+    // Count neighbours (N, L and R values) for each particle
+    for (xg=0; xg<xCells; xg++) {							                  // Step through the grid cells (left to right)
+      for (yg=0; yg<yCells; yg++) {                             // Step through the grid cells (top to bottom)
+        for (gp=0; gp<grid[xg][yg].length; gp++) {		          // Step through the particles in each grid
+          let i=grid[xg][yg][gp];                               // Now we have a particle index and we're already in the right grid
+          for (xc=xg-1; xc<=xg+1; xc++) {                       // Iterate through the 9 neighbour cells (left to right)
+            for (yc=yg-1; yc<=yg+1; yc++) {                     // Iterate through the 9 neighbour cells (top to bottom)
+              let sxc=scope(xc,xCells), syc=scope(yc,yCells);   // Wrap screen edges, Pac-Man style
+              for (jp=0; jp<grid[sxc][syc].length; jp++) {      // Iterate through the particles in each cell			
+                let j=grid[sxc][syc][jp];                       // Finally we have both i & j
+                if (i<j) {                                      // Still not gonna check it twice
+                  let sX=scope(p[j].x-p[i].x+vw2,vw)-vw2;       // Calculate x-axis separation
+                  let sY=scope(p[j].y-p[i].y+vh2,vh)-vh2;       // Calculate y-axis separation
+                  let sD=(sX*sX)+(sY*sY);                       // Calculate the square of the separation distance
+                  if (sD<=r2) {                                 // If separation distance is less than r
+                    p[i].N++, p[j].N++;                         // Increase total neighbour counts
+                    if (sX*p[i].pSin-sY*p[i].pCos<0) p[i].R++;  // Particle j is to the right of i
+                    else p[i].L++;                              // Particle j is to the left of i
+                    if (sX*p[j].pSin-sY*p[j].pCos>0) p[j].R++;  // Particle i is to the right of j
+                    else p[j].L++;                              // Particle i is to the left of j
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+    
+    // Clear HTML canvas (or leave trails)
+    context.fillStyle='rgba(0,0,0,'+o+')';
+    context.fillRect(0,0,vw,vh);
+    
+    // Draw particles on HTML canvas then apply formula
+    for (i=0; i<n; i++) {
+      if (ps==0) {
+        // Draw vectors (arrows)
+        context.beginPath();
+        context.moveTo(p[i].x,p[i].y);
+        context.lineTo(p[i].x+(v*p[i].pCos),p[i].y+(v*p[i].pSin));
+        context.moveTo(p[i].x+(v*p[i].pCos)+((v*0.25+4)*Math.cos(p[i].phi+3.5)),p[i].y+(v*p[i].pSin)+((v*0.25+4)*Math.sin(p[i].phi+3.5)));
+        context.lineTo(p[i].x+(v*p[i].pCos),p[i].y+(v*p[i].pSin));
+        context.lineTo(p[i].x+(v*p[i].pCos)+((v*0.25+4)*Math.cos(p[i].phi-3.5)),p[i].y+(v*p[i].pSin)+((v*0.25+4)*Math.sin(p[i].phi-3.5)));
+        context.strokeStyle='hsl('+hue(p[i].N)+',100%,50%)';
+        context.stroke();
+        context.fillStyle='#FFF';
+        context.fillRect(p[i].x-1,p[i].y-1,2,2);
+        context.closePath();
+      } else {
+        if (srdpr>2) {
+          // Draw a circle
+          context.beginPath();
+          context.arc(p[i].x,p[i].y,sr,0,tau);
+          context.fillStyle='hsl('+hue(p[i].N)+',100%,50%)';
+          context.fill();
+          context.closePath();
+        } else {
+          // Draw a small square
+          context.fillStyle='hsl('+hue(p[i].N)+',100%,50%)';
+          context.fillRect(p[i].x-sr,p[i].y-sr,sr2,sr2);
+        }
+        
+        // Draw radius, orientation and gamma (r, phi and g)
+        if ((n<=500) && (f<=15)) {
+          context.beginPath();
+          context.moveTo(p[i].x,p[i].y);
+          context.lineTo(p[i].x+(v*Math.cos(p[i].phi)),p[i].y+(v*Math.sin(p[i].phi)));
+          context.strokeStyle='#666';
+          context.stroke();
+          context.closePath();
+          context.beginPath();
+          context.arc(p[i].x,p[i].y,r,0,tau);
+          context.strokeStyle='rgba(255,255,255,'+(50/(n*f))+')';
+          context.stroke();
+          context.closePath();
+        }
+      }
+      
+      // Append histogram data
+      if (analyse) {
+        if (hist[0][p[i].N]!==undefined) hist[0][p[i].N]++; else hist[0][p[i].N]=1;
+        if (hist[1][p[i].L]!==undefined) hist[1][p[i].L]++; else hist[1][p[i].L]=1;
+        if (hist[2][p[i].R]!==undefined) hist[2][p[i].R]++; else hist[2][p[i].R]=1;
+      }
+          
+      // Apply changes in orientation
+      let delta_phi=a+(b*p[i].N*Math.sign(p[i].R-p[i].L));  // delta_phi = alpha + beta × N × sign(R - L)
+      p[i].phi=scope(p[i].phi+delta_phi,tau);               // Turn right delta_phi
+      
+      // Move forward v
+      p[i].x+=(v*p[i].pCos);
+      p[i].y+=(v*p[i].pSin);
+      
+      // Wrap screen edges, Pac-Man style
+      p[i].x=scope(p[i].x,vw);
+      p[i].y=scope(p[i].y,vh);
+    }
+    
+    // Analytics
+    if (analyse) {
+      // framesPerSecond();
+      // orbitals();
+      histogram();
+    }
+  }
