d86e9417976a8427dc806b28243af43f83d7d36f
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-02 16:28:43 +0300

    action providers template

---
+++ Math_Chaos/Particle_Life/src/main/java/com/particle_life/app/cursors/CursorProvider.java
@@ -0,0 +1,20 @@
+package com.particle_life.app.cursors;
+
+import com.particle_life.app.selection.InfoWrapper;
+import com.particle_life.app.selection.InfoWrapperProvider;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CursorProvider implements InfoWrapperProvider<CursorShape> {
+    @Override
+    public List<InfoWrapper<CursorShape>> create() {
+        List<InfoWrapper<CursorShape>> list = new ArrayList<>();
+
+        list.add(new InfoWrapper<>("Circle", "Cursor with round shape.", new CircleCursorShape()));
+        list.add(new InfoWrapper<>("Square", "Cursor with square shape.", new SquareCursorShape()));
+        list.add(new InfoWrapper<>("Infinity", "Cursor that always selects all particles.", new InfinityCursorShape()));
+
+        return list;
+    }
+}
