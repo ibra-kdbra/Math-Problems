37edd963c6f134e939c33ed9ec5f95afbeaf5dea
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-07 02:58:32 +0300

    params for the simulation

---
+++ Math_Chaos/Buddhabrot_Computation/params.h
@@ -0,0 +1,18 @@
+#pragma once
+
+// Hold the parameters for the simulation
+static struct Params
+{
+    const int dim = 1024;
+    const int min_depth = 10000;
+    const int depth = 100000;
+    const int samples = 100000;
+    const int samples_per_iteration = 1000;
+
+    const double fuzz = 0.00001;
+    const double threshold = 5.0;
+    const double offset_x = 2.2;
+    const double offset_y = 1.25;
+    const double x_range = 2.5;
+    const double y_range = 2.5;
+} p;
