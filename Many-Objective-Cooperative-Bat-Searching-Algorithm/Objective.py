85f3d1d797644a87fa2654ca465da642053727a5
ibra-kdbra <git-repo-develop@outlook.com>
2024-04-08 23:59:49 +0300

    removed

--- Many-Objective-Cooperative-Bat-Searching-Algorithm/Objective.py
+++
@@ -1,7 +0,0 @@
-# Abstract Objective Class
-
-class Objective:
-    def __init__(self,name, parameterIndex=[], function=lambda: 0):
-        self.name = name
-        self.parameterIndex = parameterIndex
-        self.function = function
