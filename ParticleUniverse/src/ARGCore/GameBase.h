f9508d8fae209088af9952313c603b7694a2fd70
ibra-kdbra <git-repo-develop@outlook.com>
2024-06-02 17:13:57 +0300

    game base header

---
+++ ParticleUniverse/src/ARGCore/GameBase.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "TimingManager.h"
+
+#include <string>
+#include <list>
+#include <cmath>
+
+struct ALLEGRO_EVENT_QUEUE;
+
+class GameBase
+{
+public:
+	GameBase(bool _requireMouse, int _advanceLimit);
+	void MainLoop();
+
+protected:
+	virtual void Advance(float _deltaTime) = 0;
+	virtual void Render() = 0;
+	virtual void OnClose() {}
+
+	TimingManager* m_timingManager;
+
+private:
+	void ScreenSwap();
+
+	bool m_showFPS = false;
+	int m_advanceLimit = 0;
+	ALLEGRO_EVENT_QUEUE* m_eventQueue = nullptr;
+};
