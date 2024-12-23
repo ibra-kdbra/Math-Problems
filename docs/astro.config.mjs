d2e4375e2421d9996886253e109fed87de7977f4
ibra-kdbra <git-repo-develop@outlook.com>
2024-12-23 23:07:07 +0300

    packages, deps, providers, configurations

---
+++ docs/astro.config.mjs
@@ -0,0 +1,72 @@
+import mdx from '@astrojs/mdx'
+import react from '@astrojs/react'
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+import { defineConfig } from 'astro/config'
+import astroDocs from './integrations/astroDocs'
+
+export default defineConfig({
+  output: 'hybrid',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+  }),
+  prefetch: true,
+  integrations: [
+    react(),
+    tailwind(),
+    mdx(),
+    astroDocs({
+      anchors: [
+        {
+          name: 'Community',
+          icon: 'discord',
+          url: '/',
+        },
+        {
+          name: 'Blog',
+          icon: 'newspaper',
+          url: '/',
+        },
+      ],
+      navigation: [
+        {
+          group: 'Getting Started',
+          type: 'getting-started',
+          pages: [
+            { name: 'Introduction', url: '/' },
+            { name: 'Quickstart', url: '/getting-started/quickstart' },
+          ],
+        },
+        {
+          group: 'Essentials',
+          type: 'essentials',
+          pages: [
+            { name: 'Creating pages', url: '/essentials/creating-pages' },
+            { name: 'Theming', url: '/essentials/theming' },
+            { name: 'Components', url: '/essentials/components' },
+            { name: 'Images', url: '/essentials/images' },
+            { name: 'Deployments', url: '/essentials/deployments' },
+          ],
+        },
+        {
+          group: 'Advanced',
+          type: 'advanced',
+          pages: [
+            { name: 'Pull Requests', url: '/advanced/pull-request' },
+          ]
+        }
+      ],
+    }),
+  ],
+  markdown: {
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true,
+    },
+  },
+  content: {
+    collections: ['getting-started', 'essentials', 'my-new-page'],
+  },
+})
---
+++ docs/tailwind.config.ts
@@ -0,0 +1,83 @@
+const { fontFamily } = require('tailwindcss/defaultTheme')
+import { type Config } from 'tailwindcss'
+
+const config = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: ['selector', '[data-theme="dark"]'],
+  theme: {
+    extend: {
+      colors: {
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        warning: {
+          DEFAULT: 'hsl(var(--warning))',
+          foreground: 'hsl(var(--warning-foreground))',
+        },
+        callout: {
+          DEFAULT: 'hsl(var(--callout))',
+          foreground: 'hsl(var(--callout-foreground))',
+        },
+        note: {
+          DEFAULT: 'hsl(var(--note))',
+          foreground: 'hsl(var(--note-foreground))',
+        },
+        info: {
+          DEFAULT: 'hsl(var(--info))',
+          foreground: 'hsl(var(--info-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: '0' },
+          to: { height: 'var(--accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--accordion-content-height)' },
+          to: { height: '0' },
+        },
+      },
+      fontFamily: {
+        sans: ['Inter', ...fontFamily.sans],
+      },
+    },
+  },
+  plugins: [require('tailwindcss-animate'), require('@tailwindcss/typography')],
+} satisfies Config
+
+export default config
