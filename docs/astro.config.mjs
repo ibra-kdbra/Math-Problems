784001354da96188f995e1e05ab32493f423bd6d
ibra-kdbra <git-repo-develop@outlook.com>
2025-04-30 01:20:23 +0300

    update versions based on the template I done

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -6,71 +6,19 @@ import { defineConfig } from 'astro/config'
 export default defineConfig({
   integrations: [
     starlight({
-      title: 'astrodocs',
+      title: 'starmint',
       logo: {
         light: './src/assets/dark-logo.svg',
         dark: './src/assets/light-logo.svg',
       },
       customCss: ['./src/styles/custom.css', './src/styles/tailwind.css'],
-      social: {
-        github: 'https://github.com/withastro/starlight',
-      },
       components: {
         Header: './src/components/Header.astro',
       },
-      sidebar: [
-        {
-          label: 'Getting Started',
-          items: [
-            {
-              label: 'Introduction',
-              slug: 'getting-started/introduction',
-            },
-            {
-              label: 'Quickstart',
-              slug: 'getting-started/quickstart',
-            },
-          ],
-        },
-        {
-          label: 'Equation Field',
-          items: [
-            {
-              label: 'Differentiator',
-              slug: 'equations-field/differentiator',
-            },
-            {
-              label: 'Equa Plot',
-              slug: 'equations-field/equa-plot',
-            },
-          ],
-        },
-        {
-          label: 'Encoder Decoder',
-          items: [
-            {
-              label: 'DnsLib',
-              slug: 'encoder-decoder/dnslib',
-            },
-            {
-              label: 'Image encoding',
-              slug: 'encoder-decoder/img-encoding',
-            },
-          ],
-        },
-        {
-          label: 'Mandlebrot',
-          items: [
-            {
-              label: 'Fractal Gh',
-              slug: 'mandlebrot/fractal-gh',
-            },
-            {
-              label: 'Mandlebrot Coloring',
-              slug: 'mandlebrot/coloring',
-            },
-          ],
-        },
+      sidebar: [{ label: "Getting Started", autogenerate: { directory: "getting-started" } },
+      { label: "Equations Field", autogenerate: { directory: "equations-field" } },
+      { label: "Encoder Decoder", autogenerate: { directory: "encoder-decoder" } },
+      { label: "Mandlebrot", autogenerate: { directory: "mandlebrot" } },
       ],
     }),
     tailwind(),
