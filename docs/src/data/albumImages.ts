e6e3dce43fa9467dcdbc2e249a53d20d481fb914
ibra-kdbra <git-repo-develop@outlook.com>
2025-05-24 01:04:01 +0300

    update for the images

--- docs/src/data/albumImages.ts
+++ docs/src/data/albumImages.ts
@@ -1,4 +1,14 @@
+export const ColoringMandlebrotSamples = Array.from({ length: 2 }, (_, i) => ({
+  src: `/imgs/mandlebrot_coloring/sample_${i}.png`,
+  alt: `Sample ${i}`
+}))
+
 export const buddhabrotSamples = Array.from({ length: 4 }, (_, i) => ({
   src: `/imgs/buddhabrot_computation/sample_${i}.jpg`,
   alt: `Sample ${i}`
+}));
+
+export const fractalghSamples = Array.from({ length: 9 }, (_, i) => ({
+  src: `/imgs/fractal_gh/fractal-${i}.jpg`,
+  alt: `fractal ${i}`
 }));
