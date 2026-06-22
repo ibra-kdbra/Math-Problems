a9363156be959754771d9152e993cfa025e5f3ea
ibra-kdbra <git-repo-develop@outlook.com>
2026-06-23 00:25:27 +0300

    Update To_Topsis.py

--- Security_Algorithms/Cryptography_algortihms/Crypto_Brother/To_Topsis.py
+++ Security_Algorithms/Cryptography_algortihms/Crypto_Brother/To_Topsis.py
@@ -1,27 +1,28 @@
-#from socketserver import DatagramRequestHandler
-#from matplotlib.colors import Normalize
+# from socketserver import DatagramRequestHandler
+# from matplotlib.colors import Normalize
 import pandas as pd
 
+
 class Topsis:
-#    def __init__(self):
-#        self.topsis_pipy(hashs, hashs, len(hashs.columns), [3,1,1], ["+","-","-"])
+    #    def __init__(self):
+    #        self.topsis_pipy(hashs, hashs, len(hashs.columns), [3,1,1], ["+","-","-"])
     def Normalize(self, dataset, nCol, weights):
         for i in range(0, nCol):
             temp = 0
             # Calculating Root of Sum of squares of a particular column
             for j in range(len(dataset)):
-                temp = temp + dataset.iloc[j, i]**2
+                temp = temp + dataset.iloc[j, i] ** 2
             temp = temp**0.5
             # Weighted Normalizing a element
             for j in range(len(dataset)):
-                dataset.iat[j, i] = (dataset.iloc[j, i] / temp)*weights[i]
+                dataset.iat[j, i] = (dataset.iloc[j, i] / temp) * weights[i]
         return dataset
 
     def Calc_Values(self, dataset, nCol, impact):
         p_sln = (dataset.max().values)[0:]
         n_sln = (dataset.min().values)[0:]
         for i in range(0, nCol):
-            if impact[i] == '-':
+            if impact[i] == "-":
                 p_sln[i], n_sln[i] = n_sln[i], p_sln[i]
         return p_sln, n_sln
 
@@ -38,25 +39,27 @@ class Topsis:
         for i in range(len(temp_dataset)):
             temp_p, temp_n = 0, 0
             for j in range(0, nCol):
-                temp_p = temp_p + (p_sln[j] - temp_dataset.iloc[i, j])**2
-                temp_n = temp_n + (n_sln[j] - temp_dataset.iloc[i, j])**2
+                temp_p = temp_p + (p_sln[j] - temp_dataset.iloc[i, j]) ** 2
+                temp_n = temp_n + (n_sln[j] - temp_dataset.iloc[i, j]) ** 2
             temp_p, temp_n = temp_p**0.5, temp_n**0.5
-            score.append(temp_n/(temp_p + temp_n))
-        dataset['Topsis Score'] = score
+            score.append(temp_n / (temp_p + temp_n))
+        dataset["Topsis Score"] = score
 
         # calculating the rank according to topsis score
-        dataset['Rank'] = (dataset['Topsis Score'].rank(
-            method='max', ascending=False))
+        dataset["Rank"] = dataset["Topsis Score"].rank(method="max", ascending=False)
         dataset = dataset.astype({"Rank": int})
 
         # Writing the csv
         # print(" Writing Result to CSV...\n")
-        #dataset.to_csv("Topsis_hash.csv", index=False)
+        # dataset.to_csv("Topsis_hash.csv", index=False)
         # print(" Successfully Terminated")
         return dataset
 
+
 if __name__ == "__main__":
-    line=Topsis()
-    hashs=pd.DataFrame(pd.read_csv("sistemas_hash.csv",index_col="Algoritmos"))
-    keys=pd.DataFrame(pd.read_csv("sistemas_llaves.csv"))
-    print(line.topsis_pipy(hashs, hashs, len(hashs.columns), [3,1,1], ["+","-","-"]))
+    line = Topsis()
+    hashs = pd.DataFrame(pd.read_csv("sistemas_hash.csv", index_col="Algoritmos"))
+    keys = pd.DataFrame(pd.read_csv("sistemas_llaves.csv"))
+    print(
+        line.topsis_pipy(hashs, hashs, len(hashs.columns), [3, 1, 1], ["+", "-", "-"])
+    )
