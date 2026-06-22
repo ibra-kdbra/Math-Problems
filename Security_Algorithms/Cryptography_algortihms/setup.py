63a7df851d736a5009f4c01288a61ff0b1dafd5a
ibra-kdbra <git-repo-develop@outlook.com>
2026-06-23 00:25:27 +0300

    Update setup.py

--- Security_Algorithms/Cryptography_algortihms/setup.py
+++ Security_Algorithms/Cryptography_algortihms/setup.py
@@ -1,17 +1,16 @@
 """module can be installed and configured from here"""
+
 import json
 from os import path
-from setuptools import setup, find_packages
 from sys import version_info
 
+from setuptools import find_packages, setup
+
 VERSION = "0.1"
-CURR_PATH = "{}{}".format(path.abspath(path.dirname(__file__)), '/')
+CURR_PATH = "{}{}".format(path.abspath(path.dirname(__file__)), "/")
 
 
-
-
-def path_format(file_path=None, file_name=None, is_abspath=False,
-                ignore_raises=False):
+def path_format(file_path=None, file_name=None, is_abspath=False, ignore_raises=False):
     """
     Get path joined checking before if path and filepath exist,
      if not, raise an Exception
@@ -23,16 +22,20 @@ def path_format(file_path=None, file_name=None, is_abspath=False,
     if file_path is None or not path.exists(file_path):
         raise IOError("Path '{}' doesn't exists".format(file_path))
     if file_name is None or not path.exists(path_formatted):
-        raise IOError(
-            "File '{}{}' doesn't exists".format(file_path, file_name))
+        raise IOError("File '{}{}' doesn't exists".format(file_path, file_name))
     if is_abspath:
         return path.abspath(path.join(file_path, file_name))
     else:
         return path.join(file_path, file_name)
 
 
-def read_file(is_json=False, file_path=None, encoding='utf-8',
-              is_encoding=True, ignore_raises=False):
+def read_file(
+    is_json=False,
+    file_path=None,
+    encoding="utf-8",
+    is_encoding=True,
+    ignore_raises=False,
+):
     """Returns file object from file_path,
        compatible with all py versiones
     optionals:
@@ -73,57 +76,56 @@ def read(file_name=None, is_encoding=True, ignore_raises=False):
                 file_path=path_format(
                     file_path=CURR_PATH,
                     file_name=file_name,
-                    ignore_raises=ignore_raises))
+                    ignore_raises=ignore_raises,
+                ),
+            )
         except Exception:
             # TODO: not silence like this,
             # must be on setup.cfg, README path
-            return 'NOTFOUND'
-    return read_file(is_encoding=is_encoding,
-                     file_path=path_format(
-                         file_path=CURR_PATH,
-                         file_name=file_name,
-                         ignore_raises=ignore_raises))
+            return "NOTFOUND"
+    return read_file(
+        is_encoding=is_encoding,
+        file_path=path_format(
+            file_path=CURR_PATH, file_name=file_name, ignore_raises=ignore_raises
+        ),
+    )
 
 
 setup(
-    name='Sinacrypto',
+    name="Sinacrypto",
     version=VERSION,
-#    license=read("LICENSE", is_encoding=False, ignore_raises=True),
+    #    license=read("LICENSE", is_encoding=False, ignore_raises=True),
     packages=find_packages(),
-    package_data={'': ['settings.ini']},
+    package_data={"": ["settings.ini"]},
     include_package_data=True,
-    description='Sinacrypto test repository',
+    description="Sinacrypto test repository",
     long_description=read("README.rst"),
-    author='Ramses Ramirez',
-    author_email='ramsesrmzol@gmail.com',
-    url='-',
-    download_url='https://github.com/zandrets/Sinacrypto'.format(
-        VERSION),
-    keywords=['Sinacrypto','cryptology','Sina'],
-    install_requires=[
-        'pycryptodome',
-        'configparser'
-    ],
-    setup_requires=['requests'],
+    author="Ramses Ramirez",
+    author_email="ramsesrmzol@gmail.com",
+    url="-",
+    download_url="https://github.com/zandrets/Sinacrypto".format(VERSION),
+    keywords=["Sinacrypto", "cryptology", "Sina"],
+    install_requires=["pycryptodome", "configparser"],
+    setup_requires=["requests"],
     tests_require=[
-        'pytest',
-        'pytest-cov',
-        'pytest-html',
-        'pytest-dependency',
+        "pytest",
+        "pytest-cov",
+        "pytest-html",
+        "pytest-dependency",
     ],
-#    entry_points={
-#        'console_scripts': ['Sinacrypto=Sinacrypto.Cryptouse'],
-#    },
+    #    entry_points={
+    #        'console_scripts': ['Sinacrypto=Sinacrypto.Cryptouse'],
+    #    },
     classifiers=[
-        'Development Status :: 4 - Beta',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
 )
