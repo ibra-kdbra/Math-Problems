d7cb60f02bd93119a1785b36b48e2a990b1fb83b
ibra-kdbra <git-repo-develop@outlook.com>
2026-06-23 00:25:28 +0300

    Update chacha.py

--- Security_Algorithms/SOLvatore/ciphers/chacha.py
+++ Security_Algorithms/SOLvatore/ciphers/chacha.py
@@ -6,39 +6,44 @@ def generate_chacha(r):
     chacha = CipherDescription(size)
     n = 32
 
-    s = ['s{}'.format(i) for i in range(size)]
-    t = ['t{}'.format(i) for i in range(size)]
+    s = ["s{}".format(i) for i in range(size)]
+    t = ["t{}".format(i) for i in range(size)]
 
     v = []
     for i in range(16):
-        v.append(s[i*n:(i+1)*n])
+        v.append(s[i * n : (i + 1) * n])
 
     for j in range(r):
         for i in range(4):
-            a,b,c,d = v[0+i], v[4+(i+(j&1)*1)%4], v[8+(i+(j&1)*2)%4], v[12+(i+(j&1)*3)%4]
-            
-            chacha.add_mod(a,b,a,n,size)
+            a, b, c, d = (
+                v[0 + i],
+                v[4 + (i + (j & 1) * 1) % 4],
+                v[8 + (i + (j & 1) * 2) % 4],
+                v[12 + (i + (j & 1) * 3) % 4],
+            )
+
+            chacha.add_mod(a, b, a, n, size)
             for l in range(n):
-                chacha.apply_xor(d[l],a[l],t[l])
+                chacha.apply_xor(d[l], a[l], t[l])
             for l in range(n):
-                chacha.apply_mov(t[(l-16)%n],d[l])
-            
-            chacha.add_mod(c,d,c,n,size)
+                chacha.apply_mov(t[(l - 16) % n], d[l])
+
+            chacha.add_mod(c, d, c, n, size)
             for l in range(n):
-                chacha.apply_xor(b[l],c[l],t[l])
+                chacha.apply_xor(b[l], c[l], t[l])
             for l in range(n):
-                chacha.apply_mov(t[(l-12)%n],b[l])
-            
-            chacha.add_mod(a,b,a,n,size)
+                chacha.apply_mov(t[(l - 12) % n], b[l])
+
+            chacha.add_mod(a, b, a, n, size)
             for l in range(n):
-                chacha.apply_xor(d[l],a[l],t[l])
+                chacha.apply_xor(d[l], a[l], t[l])
             for l in range(n):
-                chacha.apply_mov(t[(l-8)%n],d[l])
-            
-            chacha.add_mod(c,d,c,n,size)
+                chacha.apply_mov(t[(l - 8) % n], d[l])
+
+            chacha.add_mod(c, d, c, n, size)
             for l in range(n):
-                chacha.apply_xor(b[l],c[l],t[l])
+                chacha.apply_xor(b[l], c[l], t[l])
             for l in range(n):
-                chacha.apply_mov(t[(l-7)%n],b[l])
-        
+                chacha.apply_mov(t[(l - 7) % n], b[l])
+
     return chacha
